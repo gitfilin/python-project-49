de5fc4ef9f33cab6c7356272e2312ac938773b3f
Filin Vladimir <vy.filin@yandex.ru>
2023-10-31 01:35:44 +0400

    fix prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,7 +6,7 @@ QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def prime():
     k = 0
-    result = random.randint(1, 100)
+    result = random.randint(2, 100)
     for i in range(2, result // 2 + 1):
         if (result % i == 0):
             k = k + 1
