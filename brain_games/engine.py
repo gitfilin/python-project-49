de7de93c7577435dcc03b3724e9d18048bacfbff
Filin Vladimir <vy.filin@yandex.ru>
2023-10-31 22:07:57 +0400

    fix all

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -6,7 +6,7 @@ def game_engine(func):
     data, right_answer, question = func()
     print('Welcome to the Brain Games!')
     name = welcome_user()
-    print(func.question)
+    print(question)
     for _ in range(0, 3, 1):
         print(f'Question: {data}')
         user_answer = prompt.string('Your answer: ')
