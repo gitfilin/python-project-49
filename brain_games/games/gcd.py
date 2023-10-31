637768ebfb6192aaabd6a2ab916b2761b7602158
Filin Vladimir <vy.filin@yandex.ru>
2023-10-31 21:08:58 +0400

    fix gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -6,7 +6,7 @@ def game_func():
     questions = 'Find the greatest common divisor of given numbers.'
     a = random.randint(1, 100)
     b = random.randint(1, 100)
-    result = f'{a} {b}'
+    questions_nod = f'{a} {b}'
     result = math.gcd(a, b)
-    answer_user = str(result)
+    answer_user = str(questions_nod)
     return result, answer_user, questions
