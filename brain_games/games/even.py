5eec80ec64a7a929a83034dfffed5c6b4d7e291f
Filin Vladimir <vy.filin@yandex.ru>
2023-10-31 21:59:26 +0400

    fix all

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -5,7 +5,7 @@ def game_func():
     questions = 'Answer "yes" if the number is even, otherwise answer "no".'
     data = random.randint(1, 100)
     if data % 2 == 0:
-        right_answer = 'yes'
+        answer_user = 'yes'
     else:
-        right_answer = 'no'
-    return data, right_answer, questions
+        answer_user = 'no'
+    return data, answer_user, questions
