37fcd67b3d98cbeb8d5cc177e29faa273f897ae6
Filin Vladimir <vy.filin@yandex.ru>
2023-11-04 18:22:28 +0400

    fix module engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -7,13 +7,14 @@ def game_engine(module):
     print(f'Hello, {name}!')
     print(module.QUESTION)
     for _ in range(0, 3, 1):
-        print(f'Question: {module.data}')
+        data, right_answer = module.generates_example()
+        print(f'Question: {data}')
         user_answer = prompt.string('Your answer: ')
-        if user_answer == module.right_answer:
+        if user_answer == right_answer.right_answer:
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(.", end=' ')
-            print(f"Correct answer was '{module.right_answer}'.")
+            print(f"Correct answer was '{right_answer.right_answer}'.")
             print(f"Let's try again, {name}!")
             break
         print(f"Congratulations, {name}!")
