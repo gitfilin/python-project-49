fdcabe747420a732cfc31a20cc79aec9a48c7d6f
Filin Vladimir <vy.filin@yandex.ru>
2023-11-04 18:28:26 +0400

    modified:   brain_games/engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -10,11 +10,11 @@ def game_engine(module):
         data, right_answer = module.generates_example()
         print(f'Question: {data}')
         user_answer = prompt.string('Your answer: ')
-        if user_answer == right_answer.right_answer:
+        if user_answer == right_answer:
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(.", end=' ')
-            print(f"Correct answer was '{right_answer.right_answer}'.")
+            print(f"Correct answer was '{right_answer}'.")
             print(f"Let's try again, {name}!")
             break
         print(f"Congratulations, {name}!")
